3ae82dfc70275da251f4815b20a4ac5b6108dcbf
aless <alessia88@hotmail.com>
2023-12-14 16:49:36 -0600

    xd

--- AcademiaFS.Proyecto.Consola/Program.cs
+++ AcademiaFS.Proyecto.Consola/Program.cs
@@ -20,5 +20,48 @@ if(username != null && password != null)
 {
     var response = await login.IniciarSesion(username, password);
     if(response.data != null)
+    {
         usuario = JsonSerializer.Deserialize<UsuarioDto>(response.data.ToString());
+
+        Console.Clear();
+
+        while (true)
+        {
+            Console.WriteLine("Menú principal\n");
+            Console.WriteLine("--- Colaboradores ---");
+            Console.WriteLine("1. Listado de colaboradores");
+            Console.WriteLine("2. Ingresar colaborador\n");
+            Console.WriteLine("--- Transportistas ---");
+            Console.WriteLine("3. Listado de transportistas");
+            Console.WriteLine("4. Ingresar transportista\n");
+            Console.WriteLine("--- Viajes ---");
+            Console.WriteLine("5. Listado de viajes");
+            Console.WriteLine("6. Ingresar viaje");
+            Console.WriteLine("7. Reporte\n");
+            Console.WriteLine("8. Salir");
+
+            string opcion = Console.ReadLine();
+
+            switch (opcion)
+            {
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "5":
+                    break;
+                case "6":
+                    break;
+                case "7":
+                    break;
+                case "8":
+                    return;
+            }
+        }
+    }
+        
 }
