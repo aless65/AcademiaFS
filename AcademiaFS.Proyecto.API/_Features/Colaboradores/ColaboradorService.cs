e10e6cdc387ebdf65c4c0903cdb247c5b5925837
aless65 <140451736+SIMEXPRO@users.noreply.github.com>
2023-12-13 20:42:17 -0600

    validó

--- AcademiaFS.Proyecto.API/_Features/Colaboradores/ColaboradorService.cs
+++ AcademiaFS.Proyecto.API/_Features/Colaboradores/ColaboradorService.cs
@@ -1,6 +1,6 @@
 ﻿using AcademiaFS.Proyecto.API._Features.Colaboradores.Entities;
 using AcademiaFS.Proyecto.API.Infraestructure.SistemaViajes.Maps;
-using Farsiman.Application.Core.Standard.DTOs;
+//using Farsiman.Application.Core.Standard.DTOs;
 using Microsoft.OpenApi.Any;
 using System;
 
@@ -35,16 +35,14 @@ namespace AcademiaFS.Proyecto.API._Features.Colaboradores
                 //    {
                 //        return Respuesta.Fault<object>("Todas las distancias deben ser mayor que 0 y menor que 50", "402");
                 //    }
-                        
+
                 //}
 
                 if (colaboradores.sucursalesXColaboradores.Count() != colaboradores.sucursalesXColaboradores.Where(x => x.suco_DistanciaKm > 0 && x.suco_DistanciaKm < 51).ToList().Count())
                     return Respuesta.Fault<object>("Todas las distancias deben ser mayor que 0 y menor que 50", "402");
 
-                if (colaboradores.sucursalesXColaboradores.Count() == colaboradores.sucursalesXColaboradores.GroupBy(i => new {i.sucu_Id})
-                    .Where(g => g.Count() > 1)
-                    .Select(g => g.Key)
-                    .Count())
+                if (colaboradores.sucursalesXColaboradores.Select(g => g.sucu_Id).Count() !=
+                    colaboradores.sucursalesXColaboradores.Select(g => g.sucu_Id).Distinct().Count())
                     return Respuesta.Fault<object>("No puede ingresar dos veces la misma sucursal");
 
                 _db.Colaboradores.Add(colaboradores);
@@ -53,13 +51,49 @@ namespace AcademiaFS.Proyecto.API._Features.Colaboradores
 
                 _db.SaveChanges();
 
-                return Respuesta.Success<object>(_db.SucursalesXColaboradores.ToList(), "Operación exitosa", "200"); 
+                return Respuesta.Success<object>(_db.SucursalesXColaboradores.ToList(), "Operación exitosa", "200");
             }
             catch
             {
-                return Respuesta.Fault<object>("Intente más tarde", "500");
+                return Respuesta.Fault<object>("Intente más tarde", "500"); 
             }
         }
+
+        //public string InsertarColaboradores(tbColaboradores colaboradores)
+        //{
+        //    try
+        //    {
+        //        //foreach (var item in colaboradores.sucursalesXColaboradores)
+        //        //{
+        //        //    if(item.suco_DistanciaKm < 1 || item.suco_DistanciaKm > 50)
+        //        //    {
+        //        //        return Respuesta.Fault<object>("Todas las distancias deben ser mayor que 0 y menor que 50", "402");
+        //        //    }
+
+        //        //}
+
+        //        if (colaboradores.sucursalesXColaboradores.Count() != colaboradores.sucursalesXColaboradores.Where(x => x.suco_DistanciaKm > 0 && x.suco_DistanciaKm < 51).ToList().Count())
+        //            return "Todas las distancias deben ser mayor que 0 y menor que 50";
+
+        //        var prueba = colaboradores.sucursalesXColaboradores.Select(g => g.sucu_Id).ToList();
+
+        //        if (colaboradores.sucursalesXColaboradores.Select(g => g.sucu_Id).Count() != 
+        //            colaboradores.sucursalesXColaboradores.Select(g => g.sucu_Id).Distinct().Count())
+        //            return "No puede ingresar dos veces la misma sucursal";
+
+        //        _db.Colaboradores.Add(colaboradores);
+
+        //        _db.SucursalesXColaboradores.AddRange(colaboradores.sucursalesXColaboradores);
+
+        //        _db.SaveChanges();
+
+        //        return _db.SucursalesXColaboradores.ToList().ToString();
+        //    }
+        //    catch
+        //    {
+        //        return "Intente más tarde";
+        //    }
+        //}
     }
 }
  
